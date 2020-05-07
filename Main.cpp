d3ac4c66cd3df30c8652bec5f24037d36770fb5e
rosicley <rosicley@usp.br>
2020-05-07 18:50:29 -0300

    GMSH OK

--- Main.cpp
+++ Main.cpp
@@ -14,9 +14,40 @@ int main(int argc, char **args)
     MPI_Comm_rank(PETSC_COMM_WORLD, &rank);
     MPI_Comm_size(PETSC_COMM_WORLD, &size);
 
-    GlobalSolid *problem = new GlobalSolid;
+    Geometry *solid = new Geometry;
 
-    problem->dataReading("parameters.txt", "properties.txt", "doublecrack.txt", "doublecrack3.msh", true);
+    Point *p0 = solid->addPoint({10.0, 0.0});
+    Point *p1 = solid->addPoint({20.0, 0.0});
+    Point *p2 = solid->addPoint({20.0, 0.1250});
+    Point *p3 = solid->addPoint({10.0, 0.1250});
+
+    Line *l0 = solid->addLine({p0, p1});
+    Line *l1 = solid->addLine({p1, p2});
+    Line *l2 = solid->addLine({p2, p3});
+    Line *l3 = solid->addLine({p3, p0});
+
+    LineLoop *ll0 = solid->addLineLoop({l0, l1, l2, l3});
+
+    PlaneSurface *s0 = solid->addPlaneSurface({ll0});
+
+    solid->transfiniteLine({l0, l2}, 51);
+    solid->transfiniteLine({l1, l3}, 3);
+    solid->transfiniteSurface({s0}, "Alternated");
+
+    solid->addDirichletCondition(l1, {0.0}, {0.0});
+    solid->addNeumannCondition(p3, {}, {-640.0});
+
+    GlobalSolid *problem = new GlobalSolid;
+    problem->dataReading("parameters.txt", "properties.txt", "vigabiISO2.txt", true);
+
+    problem->generateMesh(solid, "T6", "AUTO", "aaaaaa", true, false);
+
+    problem->addBlending({l0, l2, l3}, 0.025);
+
+    if (rank == 0)
+    {
+        problem->exportMirror();
+    }
 
     std::string planeState = problem->getPlaneState(); //EPD OR EPT
     std::string type = problem->getAnalysisType();     //DYNAMIC OR STATIC
@@ -29,7 +60,7 @@ int main(int argc, char **args)
         }
         else if (type == "DYNAMIC")
         {
-           problem->solveDynamicProblem();
+            problem->solveDynamicProblem();
         }
         else
         {
